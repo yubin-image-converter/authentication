f5bc51ab7d367c0d86f9fb56cbc2228eb996db76
YubinShin <shin.yubin18@gmail.com>
2025-04-16 21:57:34 +0900

    feat: static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 
 import { IdsControllerModule } from './ids/ids.controller.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [IdsControllerModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'), // index.html 경로
+    }),
+    // IdsControllerModule
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Authentication API')
     .setDescription('Auth service + ULID ID generator')
     .setVersion('1.0')
-    // .addTag('ids')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  const clientPath = path.join(__dirname, '..', 'client');
+
+  // 정적 파일 서빙 (HTML, CSS, JS)
+  app.useStaticAssets(clientPath);
+
+  // 👇👇 여기가 핵심 👇👇
+  // Nest의 라우팅이 아니라 Express 레벨에서 fallback 처리
+  app.use('/', express.static(clientPath));
+  app.use((req, res, next) => {
+    res.sendFile(path.join(clientPath, 'index.html'));
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
