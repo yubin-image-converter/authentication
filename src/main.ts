8339825e73afcf17b45b980e4f73ab89de3eefac
Yubin Shin <shin.yubin18@gmail.com>
2025-04-16 11:50:57 +0900

    feat: initialize app module

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication API')
+    .setDescription('Auth service + ULID ID generator')
+    .setVersion('1.0')
+    // .addTag('ids')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
