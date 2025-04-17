9546f90140ebe23a809c39ef5c4f3ae45d9d249c
YubinShin <shin.yubin18@gmail.com>
2025-04-17 09:10:36 +0900

    feat: add global middlewares and validation setup
    - Applied security middlewares: `helmet`, `cors`, and `hpp`
    - Enabled global validation with `class-validator`
    - Configured Nest app bootstrap in `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import hpp from 'hpp';
 
 import { AppModule } from './app.module';
 
@@ -10,11 +13,25 @@ async function bootstrap() {
     .setTitle('Authentication API')
     .setDescription('Auth service + ULID ID generator')
     .setVersion('1.0')
-    // .addTag('ids')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(helmet());
+  app.use(hpp());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
